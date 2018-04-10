d942c1a382f696069c0421f2017aef1bcfb6ac63
playbar <hgl868@126.com>
2018-04-10 20:39:49 +0800

    添加说明文档

--- app/src/main/cpp/log.h
+++ app/src/main/cpp/log.h
@@ -5,13 +5,9 @@
 #ifndef HELLO_JNI_MY_LOG_H
 #define HELLO_JNI_MY_LOG_H
 
+#include <unistd.h>
 #include <android/log.h>
-
-//#define LOG_TAG "tfandroid"
-#define LOG_TAG (strrchr(__FILE__, '/') + 1)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#include <time.h>
 
 #define MY_LOG_LEVEL_VERBOSE    1
 #define MY_LOG_LEVEL_DEBUG      2
@@ -22,7 +18,7 @@
 #define MY_LOG_LEVEL_SILENT     7
 
 #ifndef MY_LOG_TAG
-#define MY_LOG_TAG "Retro"
+#define MY_LOG_TAG "test"
 #endif
 
 #ifndef MY_LOG_LEVEL
@@ -30,6 +26,15 @@
 #endif
 
 #define MY_LOG_NOOP (void)0
+
+#define LOG_TAG (strrchr(__FILE__, '/') + 1)
+//#define LOG_TAG "mbgl_android"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG,  __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG,  __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG,  __VA_ARGS__)
+
+#define LOGETAG(TAG, ...) __android_log_print(ANDROID_LOG_ERROR, TAG,  __VA_ARGS__)
 
 #define MY_LOG_PRINT(level, fmt, ...) \
     __android_log_print(level, MY_LOG_TAG, fmt, __VA_ARGS__ )
@@ -88,5 +93,48 @@
 #else
 #define MY_LOG_ASSERT(...) MY_LOG_NOOP
 #endif
+
+inline uint64_t GetTimeNano()
+{
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    uint64_t result = t.tv_sec * 1000000000LL + t.tv_nsec;
+    return result;
+}
+
+
+inline void ShowFPS()
+{
+#define INTERVALTIME 1000
+    static unsigned int frameCounter = 0;
+    static unsigned int prevTimeMs = 0;
+    static unsigned int lastTimeMS = 0;
+    static float maxFPS = 0;
+    static float minFPS = 0;
+
+    unsigned int currentTimeMs = GetTimeNano() * 1e-6;
+    float everyFPS =  1000.0f / (float)(currentTimeMs - lastTimeMS);
+    if( everyFPS > maxFPS ){
+        maxFPS = everyFPS;
+    }
+    if( everyFPS < minFPS )
+    {
+        minFPS = everyFPS;
+    }
+    lastTimeMS = currentTimeMs;
+    frameCounter++;
+    if (currentTimeMs - prevTimeMs > INTERVALTIME)
+    {
+        float elapsedSec = (float)(currentTimeMs - prevTimeMs) / 1000.0f;
+        float currentFPS = (float)frameCounter / elapsedSec;
+        LOGI("submit, FPS: %0.2f, maxFPS: %0.2f, minFPS: %0.2f",  currentFPS, maxFPS, minFPS);
+
+        minFPS = currentFPS;
+        maxFPS = currentFPS;
+        frameCounter = 0;
+        prevTimeMs = currentTimeMs;
+    }
+}
+
 
 #endif //HELLO_JNI_MY_LOG_H
--- doc/struct.md
+++ doc/struct.md
@@ -6,5 +6,16 @@ java层
 Graph
 TensorFlowInferenceInterface
 
+
+# 图片识别流程：
+ 1. ClassifierActivity.java     -> processImageRGBbytes
+ 2. TensorFlowImageClassifier.java ->recognizeImage
+ 3. TensorFlowInferenceInterface.java -> feed
+ 4. TensorFlowInferenceInterface.java -> run
+ 5. Session.Runner->run->runHelper
+ 6. Java_org_tensorflow_Session_run
+ 7. TF_SessionRun
+
+
 核心基类 OpKernel
 
